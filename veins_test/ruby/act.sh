403af417eaac5bb9ca3e9c559e4a22f73b3b469b
Tim Frison <frison@gmail.com>
2026-02-15 22:17:28 -0700

    ruby tests for stdin

--- veins_test/ruby/act.sh
+++ veins_test/ruby/act.sh
@@ -4,5 +4,9 @@ chmod +x ./*.rb 2>/dev/null || true
 ./test.rb
 
 echo ""
+echo "=== Test: Stdin ==="
+echo "hello" | ./stdin_upper.rb
+
+echo ""
 echo "=== Test: Argument passing ==="
 ./echo_args.rb foo bar baz
